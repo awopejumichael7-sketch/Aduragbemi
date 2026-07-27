eef156022e6c9b59b99fdcf70e6a5adbe36cea4b
awopejumichael7-sketch <awopejumichael7@gmail.com>
2026-07-27 22:32:53 +0100

    Update icon paths in service worker

--- service-worker.js
+++ service-worker.js
@@ -7,10 +7,10 @@ const CORE_ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-maskable-192.png",
-  "./icons/icon-maskable-512.png"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./icon-maskable-192.png",
+  "./icon-maskable-512.png"
 ];
 
 // Install: pre-cache the app shell
